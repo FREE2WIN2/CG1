32ca40fb2908c3a2629d63a0529f1400ce7b584a
FREE2WIN2 <57558727+FREE2WIN2@users.noreply.github.com>
2021-11-02 16:28:49 +0100

    implement calc surface area for each polygon type

--- exercise1/src/SurfaceArea.cpp
+++ exercise1/src/SurfaceArea.cpp
@@ -2,36 +2,28 @@
 // chair of the TU Dresden. Do not distribute! 
 // Copyright (C) CGV TU Dresden - All Rights Reserved
 
+#include <iostream>
 #include "SurfaceArea.h"
 
-#include <iostream>
 
-
-/* still dont work //TODO*/
-float ComputeSurfaceArea(HEMesh& mesh) {
+float ComputeSurfaceArea(HEMesh mesh) {
     float area = 0.0;
     /* Task 1.2.2 */
-    HEMesh::VertexIter v_it, v_end(mesh.vertices_end());
-    HEMesh::VertexFaceIter     vf_it;
 
-    /* Iterate over vertices */
-    for (v_it = mesh.vertices_begin(); v_it != v_end; ++v_it) {
+    /* Iterate over faces */
+    for (HEMesh::FaceIter vf_it = mesh.faces_begin(); vf_it != mesh.faces_end(); ++vf_it) {
+        HEMesh::FaceVertexIter fv_it = mesh.fv_iter(*vf_it);
 
-
-        /* Iterate over faces */
-        for (vf_it = mesh.vf_iter(v_it); vf_it; ++vf_it) {
-            HEMesh::PolyConnectivity::FaceVertexIter fv_it = mesh.fv_iter(vf_it);
-
-            const OpenMesh::Vec3f P = mesh.point(fv_it);
+        HEMesh::Point face_area(0, 0, 0);
+        for (int i = 0; i < mesh.valence(*vf_it); ++i) {
+            HEMesh::Point from_point = mesh.point(*fv_it);
             ++fv_it;
-            const OpenMesh::Vec3f Q = mesh.point(fv_it);
-            ++fv_it;
-            const OpenMesh::Vec3f R = mesh.point(fv_it);
-            std::cout << (((Q - P) % (R - P)).norm() * 0.5f * 0.3333f) << std::endl;
-            area += ((Q - P) % (R - P)).norm() * 0.5f * 0.3333f;    // norm: compute euclidean norm, return Scalar
+            HEMesh::Point to_point = mesh.point(*fv_it);
+            face_area += from_point.cross(to_point);
         }
-        std::cout << "area ";
-        std::cout << area << std::endl;
+        area += 0.5 * abs(face_area.length());
     }
+
+
     return area;
 }
